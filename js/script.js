e7f99a033964a91c725cc7e2de4b47edb74c54c3
williancozzi <williancozzi@gmail.com>
2020-07-13 18:39:02 -0300

    including the javacript and css details

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 window.addEventListener('load', start);
 
+var sliderRed = document.querySelector('#sliderRed');
+var sliderGreen = document.querySelector('#sliderGreen');
+var sliderBlue = document.querySelector('#sliderBlue');
+var inputRed = document.querySelector('#input-red');
+var inputGreen = document.querySelector('#input-green');
+var inputBlue = document.querySelector('#input-blue');
+var boxRGB = document.querySelector('.boxRGB');
+
+sliderRed.addEventListener('input', setColor);
+sliderGreen.addEventListener('input', setColor);
+sliderBlue.addEventListener('input', setColor);
+
 function start() {
   console.log("Page fully loaded.");
+  setColor();
 }
 
+function setColor() {
+  inputRed.value = sliderRed.value;
+  inputGreen.value = sliderGreen.value;
+  inputBlue.value = sliderBlue.value;
+  boxRGB.style.backgroundColor = `rgb(${sliderRed.value},${sliderGreen.value}, ${sliderBlue.value})`;
+}
